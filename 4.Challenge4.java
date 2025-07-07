ddc6b984cf39e9fe2ff40e30660f562309b89288
kaguaryan <ryankagua@gmail.com>
2025-07-07 12:51:26 +0300

    Create 4.Challenge4.java

---
+++ 4.Challenge4.java
@@ -0,0 +1,21 @@
+package challenges;
+import java.util.Scanner;
+
+public class Challenge_4 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int mark;
+
+        do {
+            System.out.println("What is your score?");
+            mark = input.nextInt();
+
+            if (mark < 70) {
+                System.out.println("Not a pass");
+            }
+
+        } while (mark < 70); 
+
+        System.out.println("Congratulations"); 
+    }
+}
