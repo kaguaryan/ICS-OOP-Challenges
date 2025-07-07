6433ee476254e3ee53c2f72ab4b3bbe5aa93d376
kaguaryan <ryankagua@gmail.com>
2025-07-07 12:55:46 +0300

    Create Exception.java

---
+++ Exception.java
@@ -0,0 +1,12 @@
+package challenges;
+
+public class Exception {
+    public static void main(String[] args) {
+int number1 = 100;
+int number2 = 0;
+
+int answer = number1/number2;
+
+System.out.println("Answer" + answer);
+    }
+}
