e8fefd771be976b6140037466f7bd1725fc9b638
kaguaryan <ryankagua@gmail.com>
2025-07-07 11:51:03 +0300

    Create Challenge2.java

---
+++ Challenge2.java
@@ -0,0 +1,20 @@
+package challenges;
+public class Challenge_3 {
+
+    int age = 24;
+
+    public void AgeChecker() {
+      
+        if (age < 0) {
+            System.out.println("Can't be possible");
+        } else if (age < 18) {
+            System.out.println("You are too young to enter");
+        } else {
+            System.out.println("You are old enough to enter");
+        }
+    }
+
+    public static void main(String[] args) {
+        new Challenge_3().AgeChecker();  
+    }
+}
